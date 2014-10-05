c12e6fe67c71e21458126a9669cae4c2dad8b776
justinliu2 <188193223@qq.com>
2014-10-05 15:15:12 +0800

    Project-Eurler-1

--- Project-Eurler 1.rkt
+++ Project-Eurler 1.rkt
@@ -4,10 +4,10 @@
 (define sum-of-multiples-of-3 
   (/ (* (+ 3 999) 333) 2))
 (define sum-of-multiples-of-5
-  (/ (* (+ 5 1000) 200) 2))
+  (/ (* (+ 5 995) 199) 2))
 (define sum-of-multiples-of-15
   (/ (* (+ 15 990) 66) 2))
 (define sum-of-multiples-of-3-or-5
   (- (+ sum-of-multiples-of-3 sum-of-multiples-of-5) 
      sum-of-multiples-of-15))
-sum-of-multiples-of-15
+sum-of-multiples-of-3-or-5
