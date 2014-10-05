76210b1ada249baa48f608893399373fe441dfd6
justinliu2 <188193223@qq.com>
2014-10-05 15:01:16 +0800

    Project-Eurler

---
+++ Project-Eurler 1.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Project-Eurler 1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+(define sum-of-multiples-of-3 
+  (/ (* (+ 3 999) 333) 2))
+(define sum-of-multiples-of-5
+  (/ (* (+ 5 1000) 200) 2))
+(define sum-of-multiples-of-15
+  (/ (* (+ 15 990) 66) 2))
+(define sum-of-multiples-of-3-or-5
+  (- (+ sum-of-multiples-of-3 sum-of-multiples-of-5) 
+     sum-of-multiples-of-15))
+sum-of-multiples-of-15
